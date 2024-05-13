1947e2ed3629786d65dd03ef516dbe579c3d8e03
su-san1129 <deviationvalue1023@outlook.jp>
2024-05-13 12:02:36 +0900

    Server の環境整備

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,16 +1,7 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
 
-const app = new Hono()
+const app = new Hono();
+app.get('/', (c) => c.text('Hello 🔥'));
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-const port = 3000
-console.log(`Server is running on port ${port}`)
-
-serve({
-  fetch: app.fetch,
-  port
-})
+export default app;
