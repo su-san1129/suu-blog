0d7ae88f2f5098130aea500c76a2ca437176c475
su-san1129 <deviationvalue1023@outlook.jp>
2024-05-13 19:17:48 +0900

    シンタックスハイライト実装

--- apps/client/src/components/article/Article.tsx
+++ apps/client/src/components/article/Article.tsx
@@ -1,19 +1,56 @@
+import { Card } from "antd";
+import Markdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+type CodeBlockProps = {
+  className: string;
+  children: string;
+};
+
+const CodeBlock: React.FC<CodeBlockProps> = ({
+  className,
+  children,
+  ...rest
+}) => {
+  const match = /language-(\w+)/.exec(className || "");
+  return match ? (
+    <SyntaxHighlighter
+      {...rest}
+      PreTag="div"
+      children={String(children).replace(/\n$/, "")}
+      language={match[1]}
+      style={atomDark}
+    />
+  ) : (
+    <code
+      {...rest}
+      className={className}
+      style={{
+        padding: "0 2px",
+        background: "#afb8c133",
+        borderRadius: "4px",
+      }}
+    >
+      {children}
+    </code>
+  );
+};
+
 type Props = {
   title: string;
   body: string;
 };
-const Article = ({ title, body }: Props) => (
-  <div
-    style={{
-      padding: 24,
-      minHeight: 380,
-      background: "#fff",
-      borderRadius: "12px",
-    }}
-  >
-    <h1>{title}</h1>
-    <div>{body}</div>
-  </div>
+const Article: React.FC<Props> = ({ title, body }) => (
+  <Card title={title}>
+    <Markdown
+      components={{
+        code: CodeBlock,
+      }}
+    >
+      {body}
+    </Markdown>
+  </Card>
 );
 
 export default Article;
